92687f4de82cda7b86ccdb8029e4c1ad24952b12
Yuege (Gail) Xie <yuege@ices.utexas.edu>
2022-07-14 00:57:49 -0500

    add unet parts

---
+++ networks/unet_model.py
@@ -0,0 +1,71 @@
+""" Full assembly of the parts to form the complete network """
+
+from .unet_parts import *
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        # change inchannels to 3
+        if x.size()[1] == 1:
+            x = x.repeat(1, 3, 1, 1)
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+    def load_from(self, config):
+        pass
+
+
+class UNetLatent(UNet):
+    def __init__(self, config, img_size, num_classes):
+        n_channels = config.n_channels
+        n_classes = config.n_classes
+        bilinear = config.bilinear
+        super(UNetLatent, self).__init__(n_channels, n_classes, bilinear)
+        print("Finish building UNetLatent")
+
+    def forward(self, x, return_latent=False):
+        # change inchannels to 3
+        if x.size()[1] == 1:
+            x = x.repeat(1, 3, 1, 1)
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        latents = [x5]
+        if return_latent:
+            return logits, latents
+        else:
+            return logits
